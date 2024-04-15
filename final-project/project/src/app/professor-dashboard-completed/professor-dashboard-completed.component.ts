08254c99455d9963528909040b9ca61f1b5aa7af
ryssu <166780212+ryssu@users.noreply.github.com>
2024-04-15 22:36:50 +0800

    Update professor-dashboard-completed.component.ts

--- final-project/project/src/app/professor-dashboard-completed/professor-dashboard-completed.component.ts
+++ final-project/project/src/app/professor-dashboard-completed/professor-dashboard-completed.component.ts
@@ -5,7 +5,7 @@ import { Component } from '@angular/core';
   standalone: true,
   imports: [],
   templateUrl: './professor-dashboard-completed.component.html',
-  styleUrl: './professor-dashboard-completed.component.css'
+  styleUrls: ['./professor-dashboard-completed.component.css']
 })
 export class ProfessorDashboardCompletedComponent {
 
