5d4fc7dbc9c056fbd61e7eede49c5dbec3c5ae9f
Clyde Kenneth V. Calub <ck.calub16@gmail.com>
2024-04-15 23:59:35 +0800

    added student form

---
+++ final-project/project/src/app/student-form/student-form.component.ts
@@ -0,0 +1,49 @@
+import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+
+@Component({
+  selector: 'app-student-form',
+  templateUrl: './student-form.component.html',
+  styleUrls: ['./student-form.component.css']
+})
+export class StudentFormComponent implements OnInit {
+  studentForm!: FormGroup;
+  concerns = [
+    'Thesis/Design Subject concerns',
+    'Requirements in Courses Enrolled',
+    'Mentoring/Clarification on the Topic of the Subjects Enrolled',
+    'Concerns about Electives/Tracks in the Curriculum',
+    'Concerns on Internship/OJT Matters',
+    'Concerns regarding Placement/Employment Opportunities',
+    'Concerns regarding Personal/Family, etc.'
+  ];
+
+  constructor(private fb: FormBuilder) {}
+
+  ngOnInit() {
+    this.initializeForm();
+  }
+
+  initializeForm() {
+    this.studentForm = this.fb.group({
+      studentNumber: ['', Validators.required],
+      studentName: ['', Validators.required],
+      programYear: ['', Validators.required],
+      emailAddress: ['', [Validators.required, Validators.email]],
+      phoneNumber: ['', Validators.required],
+      concern: ['', Validators.required],
+      formType: ['', Validators.required],
+      otherOffice: ['']
+    });
+  }
+
+  onSubmit() {
+    if (this.studentForm.valid) {
+      console.log("Form submitted!", this.studentForm.value);
+      // Handle form submission logic here
+    } else {
+      console.log("Form is invalid!");
+      // Handle invalid form
+    }
+  }
+}
