da7bc452353dec82a0dc2280fc57f2c176c3f8df
drgimatt <boxrox12@gmail.com>
2024-04-23 06:50:42 +0800

    added signout logic for management

--- final-project/project/src/app/management/management.component.ts
+++ final-project/project/src/app/management/management.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { Router } from '@angular/router';
+import { DataService } from '../data.service';
 
 @Component({
   selector: 'app-management',
@@ -7,9 +8,16 @@ import { Router } from '@angular/router';
   styleUrls: ['./management.component.css']
 })
 export class ManagementComponent {
-  constructor(private router: Router) { }
+  constructor(private router: Router, private dataService: DataService) { }
 
   navigateToAdminStudentForm() {
     this.router.navigate(['/admin-student-form']);
+  }
+
+  onSignOut() {
+    this.dataService.removeDataPersistent('model');
+    this.dataService.removeDataPersistent('account');
+    this.router.navigate(['/index']);
+  
   }
 }
