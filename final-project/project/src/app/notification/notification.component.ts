ba93f252f2ba384dd02a6f8d59b5f5b80dec6c88
Vatrix1234 <31158352+Vatrix1234@users.noreply.github.com>
2024-04-28 21:20:44 +0800

    Added notifications page

---
+++ final-project/project/src/app/notification/notification.component.ts
@@ -0,0 +1,23 @@
+import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+import { DataService } from '../data.service';
+
+@Component({
+  selector: 'app-notification',
+  templateUrl: './notification.component.html',
+  styleUrls: ['./notification.component.css']
+})
+export class NotificationComponent {
+  constructor(private router: Router, private dataService: DataService) { }
+
+  navigateToAdminStudentForm() {
+    this.router.navigate(['/admin-student-form']);
+  }
+
+  onSignOut() {
+    this.dataService.removeDataPersistent('model');
+    this.dataService.removeDataPersistent('account');
+    this.router.navigate(['/index']);
+  
+  }
+}
