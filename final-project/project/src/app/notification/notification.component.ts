611446c14dd01967a2bec60b6b5a1f49fbbe8541
Miguel Escandor <boxrox12@gmail.com>
2024-05-06 15:00:59 +0800

    fixed dismissing notifications

--- final-project/project/src/app/notification/notification.component.ts
+++ final-project/project/src/app/notification/notification.component.ts
@@ -6,6 +6,7 @@ import { Notification } from '../model/notification';
 import { Employee } from '../model/employee';
 import { Account } from '../model/account';
 import { Student } from '../model/student';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-notification',
@@ -19,11 +20,15 @@ export class NotificationComponent implements OnInit{
   account: Account
   model: Employee;
 
-  constructor(private router: Router, private dataService: DataService, private notificationService: NotificationService) { 
+  constructor(private router: Router, private dataService: DataService, private notificationService: NotificationService, private datePipe: DatePipe) { 
     this.account = this.dataService.getDataPersistent('account');
     this.model = this.dataService.getDataPersistent('model');
   }
   
+  formatDate(date: Date): string {
+    // Use DatePipe to format the date
+    return this.datePipe.transform(date, 'yyyy-MM-dd HH:mm:ss')!;
+  }
   
   ngOnInit(): void {
     this.notificationService.getUserNotifications(this.account.userID).subscribe(data => {
@@ -44,7 +49,7 @@ export class NotificationComponent implements OnInit{
     notification.append('message',specificNotification.message)
     notification.append('eventType',specificNotification.eventType)
     notification.append('hasSeenNotif', specificNotification.hasSeenNotif.toString())
-    notification.append('date', specificNotification.date.toISOString())
+    notification.append('date', this.formatDate(specificNotification.date))
     notification.append('eventUser', specificNotification.eventUser.myId.toString())
 
     const filteredEmployees = specificNotification.notifyPersons.filter(person => person instanceof Employee);
